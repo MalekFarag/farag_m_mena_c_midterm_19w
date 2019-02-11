c2c0424baef4795c01874b4cda442b3a754511e7
MalekFarag <43182938+MalekFarag@users.noreply.github.com>
2019-02-11 17:13:14 -0500

    form, trout page updated added data from python to html

--- includes/trout_data.py
+++ includes/trout_data.py
@@ -10,7 +10,7 @@ year=['2014', '2015', '2016', '2017', '2018']
 pos = np.arange(len(year))
 eggs=[150,190,160,110,230]
  
-plt.bar(pos,eggs,color='lime',edgecolor='#333')
+plt.bar(pos,eggs,color='green',edgecolor='black')
 plt.xticks(pos, year)
 plt.xlabel('Year', fontsize=16)
 plt.ylabel('Eggs Hatched', fontsize=16)
@@ -18,49 +18,26 @@ plt.title('Number of Trout Eggs Hatched',fontsize=20)
 plt.show()
 
 
-#2. fish caught per year in each creek
-n_groups = 3
-Komoka = (15, 14, 23)
-Oxbow = (10, 18, 14)
-Dingman = (13, 15, 14)
+#2. fish caught in each creek
+labels = 'Komoka', 'Oxbow', 'Dingman'
+sizes = [52, 42, 42]
+colors = ['gold', 'yellowgreen','lightskyblue']
+explode = (0.1, 0, 0)
 
-fig, ax = plt.subplots()
-index = np.arange(n_groups)
-bar_width = 0.35
-opacity = 0.8
- 
-rects1 = plt.bar(index, Komoka, bar_width,
-alpha=opacity,
-color='b',
-label='Komoka')
- 
-rects2 = plt.bar(index + bar_width, Oxbow, bar_width,
-alpha=opacity,
-color='g',
-label='Oxbow')
-
-rects3 = plt.bar(index + bar_width, Dingman, bar_width,
-alpha=opacity,
-color='y',
-label='Dingman')
- 
-plt.xlabel('Year')
-plt.ylabel('Fish Caught')
-plt.title('Fish Caught in Different Creek (per year)')
-plt.xticks(index + bar_width, ('2016', '2017', '2018'))
-plt.legend()
- 
-plt.tight_layout()
+plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+        autopct='%1.1f%%', shadow=True, startangle=140)
+/Users/m_farag/Downloads/farag_m_mena_c_midterm_19w-dev.faragm.anglers1/trout.html
+plt.axis('equal')
+plt.title('Fish Caught in Each Creek',fontsize=18)
 plt.show()
 
-
-
 #3. Total Amount of Eggs Hatched (brown + rainbow)
-rainbow = [100, 150, 100, 50, 150]
-brown = [100, 80, 70, 80, 100]
-plt.plot(year, rainbow, color='g')
+rainbow = [100, 250, 350,400, 550]
+brown = [100, 180, 250, 330, 430]
+plt.plot(year, rainbow, color='green')
 plt.plot(year, brown, color='orange')
 plt.xlabel('Year')
-plt.ylabel('Total Eggs Hatched by the Thousands')
-plt.title('Rainbow and Brown Trout Eggs Hatched')
+plt.ylabel('Eggs Hatched by the Thousands')
+plt.legend(['Rainbow Trout', 'Brown Trout'])
+plt.title('Total Rainbow and Brown Trout Eggs Hatched Through the Years')
 plt.show()
